22ee93a8047a4aa050193d0b0d3d0082b73f3a09
Libor Knappek <libor.knappek@gmail.com>
2021-09-20 23:15:14 +0200

    create item, error handling

--- app/catalog/forms.py
+++ app/catalog/forms.py
@@ -1,5 +1,7 @@
+#app/catalog/forms.py
+
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,PasswordField, BooleanField
+from wtforms import StringField,SubmitField,PasswordField, BooleanField, IntegerField, FloatField, SelectField
 #from wtforms.validators import DataRequired, Length, ValidationError, Email, EqualTo
 from wtforms import validators as val
 from app.catalog.models import Book, Publication
@@ -8,5 +10,15 @@ class EditBookForm(FlaskForm):
     title=StringField("Book Title", validators=[val.DataRequired()])
     author=StringField("Book Author", validators=[val.DataRequired()])
     format=StringField("Book Format", validators=[val.DataRequired()])
-    pages=StringField("No. of Pages", validators=[val.DataRequired()])
+    num_pages=StringField("No. of Pages", validators=[val.DataRequired()])
     submit=SubmitField("Update")
+
+class CreateBookForm(FlaskForm):
+    title=StringField("Book Title", validators=[val.DataRequired()])
+    author=StringField("Book Author", validators=[val.DataRequired()])
+    avg_rating = FloatField("Rating", validators=[val.DataRequired()])
+    format=SelectField("Book Format", validators=[val.DataRequired()], choices=["Hardcover","MassMarket Paperback","Paperback","ePub"])
+    num_pages=IntegerField("No. of Pages", validators=[val.DataRequired()])
+    image = StringField("Image URL", validators=[val.DataRequired()]) #val.url(require_tld=False, message="Invalid URL")]
+    pub_id = IntegerField("Publisher ID", validators=[val.DataRequired()])
+    submit=SubmitField("Create")
--- app/catalog/routes.py
+++ app/catalog/routes.py
@@ -1,12 +1,12 @@
+#app/catalog/routes.py
 
-import re
 from app.catalog import main
 from app import db
 #(since Model is in same package/folder/ from models import ... would suffice)
 from app.catalog.models import Publication, Book 
 from flask import render_template, flash, request, redirect, url_for
 from flask_login import login_required
-from app.catalog.forms import EditBookForm
+from app.catalog.forms import EditBookForm, CreateBookForm
 
 @main.route("/")
 @main.route("/home")
@@ -38,13 +38,13 @@ def delete_book(book_id):
 @login_required
 def edit_book(book_id):
     book = Book.query.get(book_id)
-    form = EditBookForm(obj=book)
+    form = EditBookForm(obj=book) #obj=book statement feeds the form from model (i.e. database table) for the instance to show values from db in form, form field names must be same as model class properties names to work.
 
     if form.validate_on_submit():
         book.title = form.title.data
         book.author = form.author.data
         book.format = form.format.data
-        book.num_pages = form.pages.data
+        book.num_pages = form.num_pages.data
 
         db.session.add(book)
         db.session.commit()
@@ -54,3 +54,22 @@ def edit_book(book_id):
         return redirect(url_for("main.display_books"))
     
     return render_template("edit_book.html",form=form)
+
+@main.route("/create_book/<pub_id>", methods=["GET","POST"])
+@login_required
+def create_book(pub_id):
+    form = CreateBookForm()
+    form.pub_id.data = pub_id
+    
+    if form.validate_on_submit():
+        if not Book.query.filter_by(title=form.author.data, author=form.author.data).first():
+            book = Book(form.title.data, form.author.data, form.avg_rating.data, form.format.data, form.image.data, form.num_pages.data, 
+                        form.pub_id.data)
+            db.session.add(book)
+            db.session.commit()
+            flash("Book record created")
+            return redirect(url_for("main.display_books"))
+        else:
+            flash("Book already exists in database")
+    return render_template("create_book.html", form=form)
+
